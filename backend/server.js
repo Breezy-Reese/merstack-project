cc7726c3fd9fd5bc1876011bb35587afd0068ea6
Breezy-Reese <basil59mutuku@gmail.com>
2025-10-07 13:27:08 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5176', 'http://localhost:5000'],
+  origin: ['http://localhost:5173', 'http://localhost:5176', 'http://localhost:5000', 'https://merstack-project.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
